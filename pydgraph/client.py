c17584336291ca83d8eca68367d2a42dad7e73f6
Mohit Ranka <mohitranka@gmail.com>
2016-07-05 19:51:13 +0530

    First cut of functional client with testcases

---
+++ pydgraph/client.py
@@ -0,0 +1,40 @@
+#
+# Copyright 2016 DGraph Labs, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This module contains the main user-facing methods for interacting with the
+Dgraph server over gRPC.
+"""
+
+__author__ = 'Mohit Ranka <mohitranka@gmail.com>'
+__maintainer__ = 'Mohit Ranka <mohitranka@gmail.com>'
+__version__ = '0.3'
+__status__ = 'development'
+
+from grpc.beta import implementations
+import graphresponse_pb2
+
+
+class DgraphClient(object):
+
+    def __init__(self, host, port):
+        self.channel = implementations.insecure_channel(host, port)
+        self.stub = graphresponse_pb2.beta_create_Dgraph_stub(self.channel)
+
+    def query(self, q, timeout=None):
+        request = graphresponse_pb2.Request(query=q)
+        response = self.stub.Query(request, timeout)
+        return response
+
---
+++ pydgraph/test/test_queries.py
@@ -0,0 +1,108 @@
+#
+# Copyright 2016 DGraph Labs, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__author__ = 'Mohit Ranka <mohitranka@gmail.com>'
+__maintainer__ = 'Mohit Ranka <mohitranka@gmail.com>'
+
+import unittest
+from client import DgraphClient
+
+
+class DgraphClientTestCases(unittest.TestCase):
+    TEST_HOSTNAME = 'localhost'
+    TEST_PORT = 8081
+
+    def setUp(self):
+        self.client = DgraphClient(self.TEST_HOSTNAME, self.TEST_PORT)
+
+    def test_mutation_and_query(self):
+        query_string = """
+        mutation
+        {
+            set
+            {
+                <alice> <name> \"Alice\" .
+                <greg> <name> \"Greg\" .
+                <alice> <follows> <greg> .
+            }
+        }
+
+        query
+        {
+            me(_xid_: alice)
+            {
+                follows
+                {
+                    name _xid_
+                }
+            }
+        }
+        """
+        response = self.client.query(query_string)
+        self.assertEqual(response.n.children[0].xid, "greg")
+        self.assertEqual(response.n.children[0].attribute, "follows")
+        self.assertEqual(response.n.children[0].properties[0].prop, "name")
+        self.assertEqual(response.n.children[0].properties[0].val, "Greg")
+        self.assertTrue(isinstance(response.l.parsing, basestring), 'Parsing latency is not available')
+        self.assertTrue(isinstance(response.l.pb, basestring), 'Protocol buffers latency is not available')
+        self.assertTrue(isinstance(response.l.processing, basestring), 'Processing latency is not available')
+
+    def test_mutation_and_query_two_levels(self):
+        query_string = """
+        mutation
+        {
+            set
+            {
+                <bob> <name> \"Bob\" .
+                <josh> <name> \"Josh\" .
+                <rose> <name> \"Rose\" .
+                <bob> <follows> <josh> .
+                <josh> <follows> <rose> .
+            }
+        }
+
+        query
+        {
+            me(_xid_: bob)
+            {
+                name _xid_ follows
+                {
+                    name _xid_ follows
+                    {
+                        name _xid_
+                    }
+                }
+            }
+        }
+        """
+        response = self.client.query(query_string)
+
+        self.assertEqual(len(response.n.children), 1)
+        self.assertEqual(response.n.children[0].xid, "josh")
+        self.assertEqual(response.n.children[0].attribute, "follows")
+        self.assertEqual(response.n.children[0].properties[0].prop, "name")
+        self.assertEqual(response.n.children[0].properties[0].val, "Josh")
+
+        self.assertEqual(len(response.n.children[0].children), 1)
+        self.assertEqual(response.n.children[0].children[0].xid, "rose")
+        self.assertEqual(response.n.children[0].children[0].attribute, "follows")
+        self.assertEqual(response.n.children[0].children[0].properties[0].prop, "name")
+        self.assertEqual(response.n.children[0].children[0].properties[0].val, "Rose")
+
+
+        self.assertTrue(isinstance(response.l.parsing, basestring), 'Parsing latency is not available')
+        self.assertTrue(isinstance(response.l.pb, basestring), 'Protocol buffers latency is not available')
+        self.assertTrue(isinstance(response.l.processing, basestring), 'Processing latency is not available')
+
