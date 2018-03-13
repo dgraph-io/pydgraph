913c7f7b795bdba493e7be7b50f27fc53ba8bfbd
Garvit Pahal <g10pahal@gmail.com>
2018-03-13 18:46:25 +0530

    Add protogen.py, update dependencies and refactor

--- pydgraph/client.py
+++ pydgraph/client.py
@@ -1,4 +1,3 @@
-#
 # Copyright 2016 DGraph Labs, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,11 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """
 This module contains the main user-facing methods for interacting with the
 Dgraph server over gRPC.
 """
+
 import grpc
 from pydgraph import txn
 from pydgraph import util
