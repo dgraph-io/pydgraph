e51ef77f114343ed0e2a3903ded09a5eb62ec7f2
Garvit Pahal <g10pahal@gmail.com>
2018-03-22 15:31:52 +0530

    Fix test_essentials

--- tests/test_essentials.py
+++ tests/test_essentials.py
@@ -11,17 +11,18 @@
 # limitations under the License.
 
 __author__ = 'Shailesh Kochhar <shailesh.kochhar@gmail.com>'
-__maintainer__ = 'Shailesh Kochhar <shailesh.kochhar@gmail.com>'
+__maintainer__ = 'Garvit Pahal <garvit@dgraph.io>'
 
+import unittest
 import grpc
 import json
 import logging
-import unittest
+
 from pydgraph import client
 
-from . import test_acct_upsert as integ
+from . import helper
 
-class TestEssentials(integ.DgraphClientIntegrationTestCase):
+class TestEssentials(helper.ClientIntegrationTestCase):
     """Tests the essentials of the client."""
 
     def testMutationAfterQuery(self):
@@ -30,7 +31,7 @@ class TestEssentials(integ.DgraphClientIntegrationTestCase):
         _ = self.client.query('''{firsts(func: has(first)) { uid first }}''')
 
         txn = self.client.txn()
-        mutation = txn.mutate(setnquads='_:node <first> "Node name first" .')
+        mutation = txn.mutate(set_nquads='_:node <first> "Node name first" .')
         self.assertTrue(len(mutation.uids) > 0, "Mutation did not create new node")
         created = mutation.uids.get('node')
         self.assertIsNotNone(created)
@@ -39,23 +40,6 @@ class TestEssentials(integ.DgraphClientIntegrationTestCase):
         query = '{{node(func: uid({uid:s})) {{ uid }} }}'.format(uid=created)
         reread = self.client.query(query)
         self.assertEqual(created, json.loads(reread.json).get('node')[0]['uid'])
-
-
-    def testMultipleMutationsInTxn(self):
-        """Test what happens when running multiple mutations in a txn."""
-        txn = self.client.txn()
-
-        for i in range(100):
-            m = txn.mutate('''_:node <seq> "{seq:d}" .'''.format(seq=i))
-
-        self.assertEqual(100, len(txn.keys),
-                         "Expected txn to have only 100 modified keys.")
-        key_counter = {}
-        for key in txn.keys:
-            key_counter[key] = key_counter.setdefault(key, 0) + 1
-        recurring_keys = [(k, c) for (k, c) in key_counter.items() if c > 1]
-        self.assertEqual(0, len(recurring_keys),
-                         "Expected txn not to have recurring keys")
 
 
 if __name__ == '__main__':
