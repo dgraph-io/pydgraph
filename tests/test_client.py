69848f0955b82fb69dcd37f9e400830f9623f529
Garvit Pahal <g10pahal@gmail.com>
2018-03-22 13:58:16 +0530

    Add tests for client and client util

---
+++ tests/test_client.py
@@ -0,0 +1,35 @@
+# Copyright 2018 DGraph Labs, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__author__ = 'Garvit Pahal <garvit@dgraph.io>'
+__maintainer__ = 'Garvit Pahal <garvit@dgraph.io>'
+
+import unittest
+
+from pydgraph.client import DgraphClient
+
+
+class TestDgraphClient(unittest.TestCase):
+    def test_constructor(self):
+        with self.assertRaises(ValueError):
+            DgraphClient()
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(TestDgraphClient())
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
