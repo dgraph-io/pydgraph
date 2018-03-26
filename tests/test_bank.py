3d66a5d500646ac61151679b3d8f786493885b8c
Garvit Pahal <g10pahal@gmail.com>
2018-03-26 16:07:59 +0530

    Update maintainer field

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__author__ = 'Garvit Pahal <garvit@dgraph.io>'
+__maintainer__ = 'Garvit Pahal <garvit@dgraph.io>'
+
 import unittest
 import grpc
 import json
