800901dadfa71a20193b1135849573ca8273a384
Daniel Mai <daniel@dgraph.io>
2019-01-03 12:14:06 -0800

    Update docstring for active voice.

--- tests/test_txn.py
+++ tests/test_txn.py
@@ -232,8 +232,7 @@ class TestTxn(helper.ClientIntegrationTestCase):
 
     def test_read_only_txn(self):
         """Tests read-only transactions. Read-only transactions should
-        not advance the start ts nor should mutations or commits be
-        allowed."""
+        not advance the start ts nor should allow mutations or commits."""
 
         query = '{ me() {} }'
 
