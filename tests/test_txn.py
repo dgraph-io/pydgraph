b121b83db0f12b1735073c2bc345c5e2b83d4685
Martin Martinez Rivera <martinmr@dgraph.io>
2019-03-20 11:11:58 -0700

    Initial commit.

--- tests/test_txn.py
+++ tests/test_txn.py
@@ -256,6 +256,30 @@ class TestTxn(helper.ClientIntegrationTestCase):
         with self.assertRaises(Exception):
             txn.commit()
 
+    def test_best_effort_txn(self):
+        """Tests best-effort transactions."""
+
+        helper.drop_all(self.client)
+        txn = self.client.txn()
+        _ = txn.mutate(set_obj={'name': 'Manish'})
+        start_ts1 = txn.start_ts
+        txn.commit()
+
+        query = '{ me(func: eq(name, Manish)) {name} }'
+        with self.assertRaises(Exception):
+            txn = self.client.txn(read_only=False, best_effort=True)
+
+        txn = self.client.txn(read_only=True, best_effort=True)
+        resp = txn.query(query)
+        start_ts2 = resp2.txn.start_ts
+        self.assertEqual([{'name': 'Manish'}], json.loads(resp.json).get('me'))
+        self.assertEqual(start_ts1, start_ts2)
+
+        with self.assertRaises(Exception):
+            txn.mutate(set_obj={'name': 'Manish'})
+        with self.assertRaises(Exception):
+            txn.commit()
+
     def test_conflict(self):
         """Tests committing two transactions which conflict."""
 
