81e01ff0e262391cb78c3a6d4347829069ab25ab
Martin Martinez Rivera <martinmr@dgraph.io>
2020-05-27 16:42:29 -0700

    Better error handling.
    This change adds more custom error types for connection errors and
    retriable errors. It also refactors some of the code to avoid
    duplication.

--- pydgraph/util.py
+++ pydgraph/util.py
@@ -14,6 +14,7 @@
 
 """Various utility functions."""
 
+import grpc
 import sys
 
 from pydgraph.meta import VERSION
@@ -31,6 +32,22 @@ def is_string(string):
 
     return isinstance(string, str)
 
-
 def is_jwt_expired(exception):
     return 'Token is expired' in str(exception)
+
+def is_aborted_error(error):
+    if isinstance(error, grpc._channel._Rendezvous) or \
+       isinstance(error, grpc._channel._InactiveRpcError):
+        status_code = error.code()
+        if (status_code == grpc.StatusCode.ABORTED or
+            status_code == grpc.StatusCode.FAILED_PRECONDITION):
+            return True
+    return False
+
+def is_retriable_error(error):
+    msg = str(error)
+    return 'Please retry' in msg or 'opIndexing is already running' in msg
+
+def is_connection_error(error):
+    msg = str(error)
+    return 'Unhealthy connection' in msg or 'No connection exists' in msg
