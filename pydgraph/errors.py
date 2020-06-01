61908669ea58c700bcf844b58830f6028c2b550b
Martin Martinez Rivera <martinmr@dgraph.io>
2020-06-01 16:37:59 -0700

    Better error handling. (#135)
    This change adds more custom error types for connection errors and
    retriable errors. It also refactors some of the code to avoid
    duplication.

--- pydgraph/errors.py
+++ pydgraph/errors.py
@@ -28,3 +28,26 @@ class AbortedError(Exception):
     def __init__(self):
         super(AbortedError, self).__init__(
             'Transaction has been aborted. Please retry')
+
+class RetriableError(Exception):
+    """Error thrown when the error return by Dgraph indicates the op should be retried."""
+
+    def __init__(self, exception):
+        self.exception = exception
+
+    def __str__(self):
+        return str(self.exception)
+
+class ConnectionError(Exception):
+    """Error thrown when the error return when the client has trouble connecting to Dgraph."""
+    def __init__(self, exception):
+        self.exception = exception
+
+    def __str__(self):
+        return str(self.exception)
+
+class TransactionError(Exception):
+    """Error thrown when the transaction is invalid (e.g trying to mutate in read-only mode)."""
+    def __init__(self, msg):
+        super(TransactionError, self).__init__(msg)
+
