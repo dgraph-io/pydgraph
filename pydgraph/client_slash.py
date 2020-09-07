9387e9006213dfe6f0707d0822771f046ef80f80
Anurag Sharma <anurags92@gmail.com>
2020-09-08 00:04:39 +0530

    Rename thigns

--- pydgraph/client_slash.py
+++ pydgraph/client_slash.py
@@ -21,7 +21,7 @@ from pydgraph.meta import VERSION
 from pydgraph.proto import api_pb2 as api
 
 __author__ = 'Anurag Sharma <anurag@dgraph.io>'
-__maintainer__ = 'Martin Martinez Rivera <martinmr@dgraph.io>'
+__maintainer__ = 'Arpan Gupta <arpan@dgraph.io>'
 __version__ = VERSION
 __status__ = 'development'
 
@@ -30,22 +30,22 @@ class SlashGraphQLClient(object):
     """Creates a new Client for interacting with the Slash GraphQL.
     """
 
-    def __init__(self, SlashEndpoint, APIKey):
-        if not SlashEndpoint:
+    def __init__(self, slash_end_point, api_key):
+        if not slash_end_point:
             raise ValueError('No Slash endpoint provided in SlashGraphQLClient constructor')
 
-        self._SlashEndpoint = SlashEndpoint
-        self._APIKey = APIKey
+        self._slash_end_point = slash_end_point
+        self._api_key = api_key
 
     def get_stub(self):
         """Returns Dgraph Client stub for the Slash GraphQL endpoint"""
-        url = self._SlashEndpoint.replace("/graphql", "")
+        url = self._slash_end_point.replace("/graphql", "")
         url = url.replace("https://", "")
         url_parts = url.split(".", 1)
-        HOST = url_parts[0] + ".grpc." + url_parts[1]
+        host = url_parts[0] + ".grpc." + url_parts[1]
         PORT = "443"
         creds = grpc.ssl_channel_credentials()
-        call_credentials = grpc.metadata_call_credentials(lambda context, callback: callback((("authorization", self._APIKey),), None))
+        call_credentials = grpc.metadata_call_credentials(lambda context, callback: callback((("authorization", self._api_key),), None))
         composite_credentials = grpc.composite_channel_credentials(creds, call_credentials)
-        client_stub = pydgraph.DgraphClientStub('{host}:{port}'.format(host=HOST, port=PORT), composite_credentials, options=(('grpc.enable_http_proxy', 0),))
+        client_stub = pydgraph.DgraphClientStub('{host}:{port}'.format(host=host, port=PORT), composite_credentials, options=(('grpc.enable_http_proxy', 0),))
         return client_stub
