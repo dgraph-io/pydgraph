45b3cb0695901f9a82a1b97651b984f1ced87b65
Daniel Mai <daniel@dgraph.io>
2021-08-05 08:46:53 -0700

    fix: Missing variable in from_cloud.

--- pydgraph/client_stub.py
+++ pydgraph/client_stub.py
@@ -90,17 +90,17 @@ class DgraphClientStub(object):
     # from_slash_endpoint is deprecated and will be removed in v21.07 release.
     # Use from_cloud method to connect to dgraph cloud backend.
     @staticmethod
-    def from_slash_endpoint(cloud_end_point, api_key):
-        return from_cloud(cloud_end_point, api_key)
+    def from_slash_endpoint(cloud_endpoint, api_key):
+        return from_cloud(cloud_endpoint, api_key)
 
     # Usage:
     # import pydgraph
     # client_stub = pydgraph.DgraphClientStub.from_cloud("cloud_endpoint", "api-key")
     # client = pydgraph.DgraphClient(client_stub)
     @staticmethod
-    def from_cloud(cloud_end_point, api_key):
+    def from_cloud(cloud_endpoint, api_key):
         """Returns Dgraph Client stub for the Slash GraphQL endpoint"""
-        url = urlparse(slash_end_point)
+        url = urlparse(cloud_endpoint)
         url_parts = url.netloc.split(".", 1)
         host = url_parts[0] + ".grpc." + url_parts[1]
         creds = grpc.ssl_channel_credentials()
