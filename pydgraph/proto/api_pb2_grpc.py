8cbe59dda9826e87a3acea0a8e0fafea95cfc0cd
Matt Johnson-Pint <mjp@hypermode.com>
2025-07-25 10:39:52 -0700

    chore: update grpc/protobuf

--- pydgraph/proto/api_pb2_grpc.py
+++ pydgraph/proto/api_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 import api_pb2 as api__pb2
 
-GRPC_GENERATED_VERSION = '1.71.0'
+GRPC_GENERATED_VERSION = '1.74.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
