5c308d3a980d61b9ccac9bc6d86b54040c92e434
Matthew McNeely <matthew.mcneely@gmail.com>
2025-07-29 14:51:59 -0400

    Raise an error if attempting to generate protobufs using python 3.13

--- scripts/protogen.py
+++ scripts/protogen.py
@@ -4,8 +4,17 @@
 """Runs protoc with the gRPC plugin to generate messages and gRPC stubs."""
 
 import os
+import sys
 
 from grpc_tools import protoc
+
+# Check Python version compatibility
+if sys.version_info >= (3, 13):
+    print("ERROR: Python 3.13+ requires grpcio-tools >=1.66.2, which generates")
+    print("protobufs that are incompatible with older grpcio-tools versions.")
+    print("Please use Python 3.12 or lower to generate compatible protobufs.")
+    print("Exiting without generating protobufs.")
+    sys.exit(1)
 
 dirpath = os.path.dirname(os.path.realpath(__file__))
 protopath = os.path.realpath(os.path.join(dirpath, "../pydgraph/proto"))
