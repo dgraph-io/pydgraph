60f033f23674530b1a90e70b1e3f09c4594d3c03
Michael Welles <mlwelles@gmail.com>
2025-12-30 22:05:43 -0500

    fix: move DgraphClientStub import to module level and update test mocks
    - Import DgraphClientStub at module level in client.py instead of locally
    - Update test mocks in test_connect.py to patch pydgraph.client.DgraphClientStub
    - This fixes the issue where client_stub function was shadowing the module name

--- pydgraph/client_stub.py
+++ pydgraph/client_stub.py
@@ -20,8 +20,6 @@ __maintainer__ = "Istari Digital, Inc. <dgraph-admin@istaridigital.com>"
 __version__ = VERSION
 __status__ = "development"
 
-__all__ = ["DgraphClientStub"]
-
 
 class DgraphClientStub(object):
     """Stub for the Dgraph grpc client."""
